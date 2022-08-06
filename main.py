59185f2339ef190d89e22e0175d509ebec6f4527
Bechera <66968588+Pepsiman715@users.noreply.github.com>
2022-08-06 14:48:03 -0400

    put request

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 #import dependencies
-from flask import Flask 
-from flask_restful import Api, Resource
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
 
 #create app
 app = Flask(__name__)
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="likes of the video is required", required=True)
+
+#object
+videos = {}
+
+def abort_video_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video could not be found")
+
+def abort_video_if_exist(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with that ID")
+        
 #create 1st ressource
-
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "15 minutes to get into the mainframe"}
-
-    def post(self):
-        return {"data": "15 minutes to post into the mainframe"}    
+class Video(Resource):
+    def get(self, video_id):
+        abort_video_doesnt_exist(video_id)
+        return videos[video_id]
+    
+    def put(self, video_id):
+        abort_video_doesnt_exist(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+    
+    def delete(self, video_id):
+        abort_video_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
 #add to the api
 #the second param is the api endpoint
-api.add_resource(HelloWorld, "/helloworld")
+#use angle brackets to pass in params
+api.add_resource(Video, "/video/<int:video_id>")
 
 
 #start server and application
--- test.py
+++ test.py
@@ -2,7 +2,19 @@ import requests
 
 Base = "http://127.0.0.1:5000/"
 
-#send a response to this endpoint
-reponse = requests.get(Base + "helloworld")
+data = [{"likes" : 5, "name": "How to make REST API", "views" : 76575},
+        {"likes" : 97, "name": "How to play drums", "views" : 43242},
+        {"likes" : 130, "name": "Jewelry", "views" : 104300}]
 
-print(reponse.json())
+#send a response to this endpoint
+for i in range(len(data)):
+    response = requests.put(Base + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+#send a response to this endpoint
+response = requests.delete(Base + "video/0")
+print(response)
+input()  
+response = requests.get(Base + "video/2")
+print(response.json())
